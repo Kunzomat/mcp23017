46a93586cafe59f583c456c2f1d55ca17a414b09
Andreas Brisgen <akunze_de@web.de>
2024-05-20 12:01:54 +0200

    Aktualisiere custom.ts

--- custom.ts
+++ custom.ts
@@ -1,15 +1,40 @@
-
-/**
-* Nutze diese Datei für benutzerdefinierte Funktionen und Blöcke.
-* Weitere Informationen unter https://makecode.microbit.org/blocks/custom
-*/
-
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
+const enum ADDRESS {
+    //% block=0x20
+    A20 = 0x20,               // Standardwert
+    //% block=0x21
+    A21 = 0x21,
+    //% block=0x22
+    A22 = 0x22,
+    //% block=0x23
+    A23 = 0x23
 }
+
+const enum REG_MCP {
+    //% Bitmuster um Register A zu beschreiben
+    Bitmuster_A = 0x12,
+    //% Bitmuster um Register B zu beschreiben
+    Bitmuster_B = 0x13,
+    //% Aus- / Eingaberichtung des Registers A
+    EinOderAusgabe_A = 0x00,
+    //% Aus- / Eingaberichtung des Registers B
+    EinOderAusgabe_B = 0x01,
+    //% Pullup Widerstände Register A
+    PullUp_Widerstaende_A = 0x0C,
+    //% Pullup Widerstände Register B
+    PullUp_Widerstaende_B = 0x0D
+}
+
+const enum BITS {
+    //% block=11111111
+    Alle = 0xff,
+    //% block=00000000
+    keiner = 0x00,
+    //% block=00000001
+    Bit1 = 0x01
+}
+
+let BitwertA = 0;
+let BitwertB = 0;
 
 /**
  * Custom blocks
@@ -23,16 +48,21 @@ namespace MCP23017 {
      * @param e describe parameter here
      */
     //% block
-    export function init(n: number, s: string, e: MyEnum): void {
-        // Add code here
+    export function init(): void {
+        // Alle Register auf Ausgabe stellen
+        MCP23017.writeRegister(ADDRESS.A20, REG_MCP.EinOderAusgabe_A, MCP23017.bitwert(BITS.keiner))
+        MCP23017.writeRegister(ADDRESS.A20, REG_MCP.EinOderAusgabe_B, MCP23017.bitwert(BITS.keiner))
+        // Pullup-Widerstände für saubere Signalübertragung ein!
+        MCP23017.writeRegister(ADDRESS.A20, REG_MCP.PullUp_Widerstaende_A, MCP23017.bitwert(BITS.Alle))
+        MCP23017.writeRegister(ADDRESS.A20, REG_MCP.PullUp_Widerstaende_B, MCP23017.bitwert(BITS.Alle))
+        //MCP23017.setLeds(State.Low) // alle LEDs ausschalten
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    export function writeRegister(addr: ADDRESS, reg: REG_MCP, value: number) {
+        pins.i2cWriteNumber(addr, reg * 256 + value, NumberFormat.UInt16BE)
+    }
+
+    export function bitwert(alle: BITS): number {
+        return alle
     }
 }
